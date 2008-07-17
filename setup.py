f32a8b4b5218298a4db972360ed9ae107a8c4982
Christian Zagrodnick <cz@flyingcircus.io>
2008-07-17 14:15:10 +0000

    0.7.5dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'gocept.form',
-    version = "0.8dev",
+    version = "0.7.5dev",
     author = "Christian Zagrodnick",
     author_email = "cz@gocept.com",
     description = "Extensions for zope.formlib",
